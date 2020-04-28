ae50502e424bba625c52abefaaa48d46e384a2c7
unknown <Administrator@CADR3.code.local>
2020-04-28 09:40:40 -0700

    carousel section is done

--- js/main.js
+++ js/main.js
@@ -58,5 +58,23 @@ $(document).ready(function(){
         $(".digital-content5").css("display","none");
         $(".digital-content6").css("display","none");
     })
-    
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        autoplay:true,
+        autoplayTimeout:2000,
+        margin:10,
+        nav:false,
+        dots:false,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:5
+            }
+        }
+    })
 })
