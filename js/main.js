ec1ba33be243df839f6732979831c8edc9938a41
unknown <Administrator@CADR3.code.local>
2020-05-02 23:14:25 -0700

    all pages are completed

--- js/main.js
+++ js/main.js
@@ -6,15 +6,25 @@ deliveredBtns.forEach(element => {
         $(activeBtn).removeClass("active");
     })
 });
+var ShopBtns = document.querySelectorAll(".shop-card-buttons li");
+ShopBtns.forEach(element => {
+    $(element).click(function(){
+        var activeBtn = document.querySelector(".active");
+        $(this).addClass("active");
+        $(activeBtn).removeClass("active");
+    })
+});
 
 $(document).ready(function(){
+
     $(window).scroll(function(){
-        if($("html").scrollTop() >= 180){
+        if($("html").scrollTop() >= 120){
         $(".navbar").addClass("scrolled");
         $(".navbar a").css("color","#232323");
         $(".navbar-brand2").css("display","block");
         $(".navbar-right-side ul li a i").css("color","#343434");
         $(".navbar-right-side").css("border-left","1px solid rgba(0, 0, 0, 0.05)");
+        $(".shop-card-navbar").css("margin-top","0");
         }
         else{
             $(".navbar").removeClass("scrolled");
@@ -22,9 +32,13 @@ $(document).ready(function(){
             $(".navbar-right-side ul li a i").css("color","#f8f8f8");
             $(".navbar-right-side").css("border-left","1px solid rgba(255, 255, 255, 0.1)");
             $(".navbar a").css("color","#f8f8f8");
+            $(".shop-card-navbar").css("margin-top","54px");
+            $(".shop-card-navbar a").css("color","#343434");
+            $(".shop-card-navbar .navbar-right-side").css("border-left","1px solid rgba(0, 0, 0, 0.05)");
+            $(".shop-card-navbar .navbar-right-side ul li a i").css("color","#343434");
+            $(".shop-card-navbar").css("transition","all 0.2s");
         }
     })
-    
     $(".showAll").click(function(){
         $(".digital-content1").css("display","block");
         $(".digital-content2").css("display","block");
@@ -57,7 +71,66 @@ $(document).ready(function(){
         $(".digital-content5").css("display","none");
         $(".digital-content6").css("display","none");
     })
-    $('.owl-carousel').owlCarousel({
+    $(".showAll2").click(function(){
+        $(".shop-content1").css("display","block");
+        $(".shop-content2").css("display","block");
+        $(".shop-content3").css("display","block");
+        $(".shop-content4").css("display","block");
+        $(".shop-content5").css("display","block");
+        $(".shop-content6").css("display","block");
+        $(".shop-content7").css("display","block");
+        $(".shop-content8").css("display","block");
+        $(".shop-content9").css("display","block");
+        $(".shop-content10").css("display","block");
+        $(".shop-content11").css("display","block");
+        $(".shop-content12").css("display","block");
+    })
+    $(".accessories").click(function(){
+        console.log("asas");
+        $(".shop-content1").css("display","block");
+        $(".shop-content2").css("display","none");
+        $(".shop-content3").css("display","none");
+        $(".shop-content4").css("display","block");
+        $(".shop-content5").css("display","none");
+        $(".shop-content6").css("display","none");
+        $(".shop-content7").css("display","block");
+        $(".shop-content8").css("display","none");
+        $(".shop-content9").css("display","block");
+        $(".shop-content10").css("display","none");
+        $(".shop-content11").css("display","block");
+        $(".shop-content12").css("display","none");
+    })
+    $(".apparel").click(function(){
+        console.log("asas");
+        $(".shop-content1").css("display","none");
+        $(".shop-content2").css("display","block");
+        $(".shop-content3").css("display","none");
+        $(".shop-content4").css("display","none");
+        $(".shop-content5").css("display","block");
+        $(".shop-content6").css("display","none");
+        $(".shop-content7").css("display","none");
+        $(".shop-content8").css("display","block");
+        $(".shop-content9").css("display","none");
+        $(".shop-content10").css("display","block");
+        $(".shop-content11").css("display","none");
+        $(".shop-content12").css("display","block");
+    })
+    $(".bags").click(function(){
+        console.log("asas");
+        $(".shop-content1").css("display","none");
+        $(".shop-content2").css("display","none");
+        $(".shop-content3").css("display","block");
+        $(".shop-content4").css("display","none");
+        $(".shop-content5").css("display","none");
+        $(".shop-content6").css("display","block");
+        $(".shop-content7").css("display","none");
+        $(".shop-content8").css("display","none");
+        $(".shop-content9").css("display","none");
+        $(".shop-content10").css("display","none");
+        $(".shop-content11").css("display","none");
+        $(".shop-content12").css("display","none");
+    })
+    $("#carousel .owl-carousel").owlCarousel({
         loop:true,
         autoplay:true,
         autoplayTimeout:2000,
@@ -75,5 +148,5 @@ $(document).ready(function(){
                 items:5
             }
         }
-    })
+    });
 })
